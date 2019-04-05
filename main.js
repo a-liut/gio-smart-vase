1a806add67eef488278ade5945d97496219e0646
a.liut <andrea.liut@gmail.com>
2019-04-05 17:56:26 +0200

    Registration process with a fog node

--- main.js
+++ main.js
@@ -1,33 +1,81 @@
 let moisture = 0
 let temperature = 0
 let light = 0
-
+let connected = false
+let registered = false
+let uart_data = ""
 input.onButtonPressed(Button.A, function () {
     log()
 })
-
-function showValues() {
-    basic.showString("LGT: " + light)
-    basic.showString("TMP: " + temperature)
-    basic.showString("MST: " + moisture)
-}
-
-function updateMeasurements() {
-    light = input.lightLevel()
-    temperature = input.temperature()
-    moisture = pins.digitalReadPin(DigitalPin.P0)
-}
-
 function log() {
-    serial.writeString("{")
+    serial.writeLine("{")
     serial.writeLine("\"temperature\": " + temperature + ",")
     serial.writeLine("\"light\": " + light + ",")
     serial.writeLine("\"moisture\": " + moisture + "")
     serial.writeLine("}")
 }
-
-basic.forever(function () {
-    updateMeasurements()
-    basic.pause(5000)
+function sendDataWithUART() {
+    bluetooth.uartWriteValue("temperature", temperature)
+    bluetooth.uartWriteValue("light", light)
+    bluetooth.uartWriteValue("moisture", moisture)
+}
+function displayBTStatus() {
+    if (connected) {
+        basic.showString("C")
+    } else {
+        basic.showString("D")
+    }
+}
+function updateMeasurements() {
+    light = input.lightLevel()
+    temperature = input.temperature()
+    pins.analogWritePin(AnalogPin.P1, 1023)
+    moisture = pins.digitalReadPin(DigitalPin.P0)
+    pins.analogWritePin(AnalogPin.P1, 0)
+}
+function sendRegisterRequest() {
+    serial.writeLine("Sending registration request")
+    bluetooth.uartWriteString("whoiam:vase")
+}
+bluetooth.onBluetoothDisconnected(function () {
+    serial.writeLine("Disconnected!")
+    connected = false
+    registered = false
+    displayBTStatus()
 })
-
+bluetooth.onBluetoothConnected(function () {
+    serial.writeLine("Connected!")
+    connected = true
+    displayBTStatus()
+})
+bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    uart_data = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
+    if (!registered) {
+        switch (uart_data) {
+            case "whoareyou":
+                sendRegisterRequest()
+                break;
+            case "OK":
+                registered = true
+                serial.writeLine("Registered with the fog node")
+                break;
+            default:
+                serial.writeLine("Something wrong happened: " + uart_data)
+                break;
+        }
+    } else {
+        serial.writeString("Data received: " + uart_data)
+    }
+})
+bluetooth.startUartService()
+basic.forever(function () {
+    displayBTStatus()
+    updateMeasurements()
+    if (connected && registered) {
+        serial.writeLine("Sending data using UART")
+        sendDataWithUART()
+    } else {
+        serial.writeLine("Not connected nor registered...")
+    }
+    basic.pause(2000)
+})
