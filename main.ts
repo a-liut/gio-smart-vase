b902cc285a002112d699e82ed11b3457da09ce15
a.liut <andrea.liut@gmail.com>
2019-04-12 10:42:17 +0200

    Communication bug fixed.

--- main.ts
+++ main.ts
@@ -2,14 +2,18 @@ enum STATUS { REGISTERING = 0, MONITORING = 1, COMMUNICATING = 2, WATERING = 3, 
 let status = STATUS.MONITORING
 let pump_timeout = 0
 let conf_mois_min = 0
+let temperature = 0
 let moisture = 0
 let light = 0
 let uart_data = ""
-let temperature = 0
+let connection_requested = false
 // Config - moisture
 conf_mois_min = 50
 // - pump
 pump_timeout = 5000
+function canConnect() {
+    return status != STATUS.WATERING && status != STATUS.COMMUNICATING && status != STATUS.REGISTERING
+}
 function checkWatering() {
     return temperature < conf_mois_min
 }
@@ -42,6 +46,25 @@ function displayBTStatus() {
             break;
     }
 }
+// function printStatus() {
+//     switch (status) {
+//         case STATUS.REGISTERING:
+//             serial.writeLine("R")
+//             break;
+//         case STATUS.MONITORING:
+//             serial.writeLine("M")
+//             break;
+//         case STATUS.COMMUNICATING:
+//             serial.writeLine("C")
+//             break;
+//         case STATUS.WATERING:
+//             serial.writeLine("W")
+//             break;
+//         case STATUS.WAITING:
+//             serial.writeLine("A")
+//             break;
+//     }
+// }
 function startPump() {
     pins.digitalWritePin(DigitalPin.P2, 1)
 }
@@ -49,7 +72,7 @@ function stopPump() {
     pins.digitalWritePin(DigitalPin.P2, 0)
 }
 function sendRegisterRequest() {
-    serial.writeLine("Sending registration request")
+    // serial.writeLine("Sending registration request")
     bluetooth.uartWriteString("whoiam:vase")
 }
 function sendDataWithUART() {
@@ -59,23 +82,29 @@ function sendDataWithUART() {
     bluetooth.uartWriteString("EOS")
 }
 bluetooth.onBluetoothConnected(function () {
-    displayBTStatus()
-    status = STATUS.REGISTERING
+    // serial.writeLine("A new device has connected")
+    if (canConnect()) {
+        status = STATUS.REGISTERING
+    }
+    connection_requested = !canConnect()
 })
 bluetooth.onBluetoothDisconnected(function () {
-    serial.writeLine("Disconnected!")
+    // serial.writeLine("The device has diconnected")
     status = STATUS.MONITORING
-    displayBTStatus()
+    connection_requested = false
 })
 bluetooth.onUartDataReceived(serial.delimiters(Delimiters.Dollar), function () {
     uart_data = bluetooth.uartReadUntil(serial.delimiters(Delimiters.Dollar))
-    if (status == STATUS.REGISTERING) {
+    // serial.writeLine("Status on data received:")
+    printStatus()
+    if (status == STATUS.REGISTERING || connection_requested) {
         switch (uart_data) {
             case "whoareyou":
                 sendRegisterRequest()
                 break;
             case "OK":
                 status = STATUS.COMMUNICATING
+                connection_requested = false
                 break;
             default:
                 serial.writeLine("Something wrong happened: " + uart_data)
@@ -88,6 +117,7 @@ bluetooth.onUartDataReceived(serial.delimiters(Delimiters.Dollar), function () {
 bluetooth.startUartService()
 basic.forever(function () {
     displayBTStatus()
+    // printStatus()
     switch (status) {
         case STATUS.WATERING:
             // Start the pump for watering
@@ -100,14 +130,20 @@ basic.forever(function () {
         case STATUS.MONITORING:
             updateMeasurements()
 
-            // Check if the plant needs water
-            if (checkWatering()) {
-                status = STATUS.WATERING
+            if (connection_requested) {
+                // A connection have the priority on other actions
+                status = STATUS.REGISTERING
             } else {
-                basic.pause(2000)
+                // Check if the plant needs water
+                if (checkWatering()) {
+                    status = STATUS.WATERING
+                } else {
+                    basic.pause(2000)
+                }
             }
             break;
         case STATUS.COMMUNICATING:
+            // serial.writeLine("Sending data!")
             sendDataWithUART()
             status = STATUS.WAITING
             basic.pause(2000)
