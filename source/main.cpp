26fe11668b72c30fc9fe77b6672758f456657691
a.liut <andrea.liut@gmail.com>
2019-09-14 17:44:58 +0200

    improved stability

--- source/main.cpp
+++ source/main.cpp
@@ -9,13 +9,13 @@
 
 #include "actuators/watering/MicroBitWateringActuator.h"
 
-#define UPDATE_TIMEOUT                      5000
+#define UPDATE_TIMEOUT 5000
 
-#define WATERING_EVENT_ID                   55536 // Reserved events: [1-65535]
-#define WATERING_EVENT_REQUESTED            1
-#define WATERING_EVENT_STARTED              2
-#define WATERING_EVENT_ENDED                3
-#define WATERING_TIMEOUT                    5000
+#define WATERING_EVENT_ID 55536 // Reserved events: [1-65535]
+#define WATERING_EVENT_REQUESTED 1
+#define WATERING_EVENT_STARTED 2
+#define WATERING_EVENT_ENDED 3
+#define WATERING_TIMEOUT 5000
 
 // uBit Services
 MicroBit uBit;
@@ -28,42 +28,22 @@ MicroBitWateringService *wateringService;
 MicroBitMoistureSensor moistureSensor(uBit.io.P0, uBit.io.P1);
 MicroBitWateringActuator wateringActuator(uBit.io.P2);
 
+// Images
+MicroBitImage drop("0,0,255,0, 0\n0,0,255,0,0\n0,255,0,255,0\n255,0,0,0,255\n0,255,0,255,0\n");
+MicroBitImage arrowDown("0,0,255,0, 0\n0,0,255,0,0\n255,0,255,0,255\n0,255,255,255,0\n0,0,255,0,0\n");
+
 /**
  * If true the watering operation is forced to run
  */
 bool forceWatering = false;
 
 /**
- * true when the device is connected
- */
-bool connected = false;
-
-void updateDisplay()
-{
-    if (wateringActuator.isWatering())
-    {
-        MicroBitImage smiley("0,0,255,0, 0\n0,0,255,0,0\n0,255,0,255,0\n255,0,0,0,255\n0,255,0,255,0\n");
-        uBit.display.setDisplayMode(DISPLAY_MODE_GREYSCALE);
-        uBit.display.print(smiley);
-    } else
-    {
-        if (connected)
-        {
-            uBit.display.printChar('C');
-        } else
-        {
-            uBit.display.scrollAsync("D");
-        }
-    }
-}
-
-/**
  * A listener to perform actions after a BLE device connects.
  */
 void onConnected(MicroBitEvent)
 {
-    connected = true;
-    updateDisplay();
+    uBit.display.scrollAsync('C');
+    uBit.sleep(50);
 }
 
 /**
@@ -71,8 +51,8 @@ void onConnected(MicroBitEvent)
  */
 void onDisconnected(MicroBitEvent)
 {
-    connected = false;
-    updateDisplay();
+    uBit.display.scrollAsync('D');
+    uBit.sleep(50);
 }
 
 /**
@@ -81,13 +61,14 @@ void onDisconnected(MicroBitEvent)
 bool canWater()
 {
     // No watering if the vase is actually watering the plant or there isn't enough water
-    if(wateringActuator.isWatering() || !wateringActuator.enoughWater()) {
+    if (wateringActuator.isWatering() || !wateringActuator.enoughWater())
+    {
         return false;
     }
 
-    if(forceWatering)
+    if (forceWatering)
         return true;
-    
+
     // Check moisture level for watering
     int32_t moisture = moistureSensor.getMoistureLevel();
 
@@ -107,6 +88,7 @@ void init()
 
     // This will returns 0 on the first call
     uBit.display.readLightLevel();
+    uBit.display.setDisplayMode(DISPLAY_MODE_BLACK_AND_WHITE_LIGHT_SENSE);
 }
 
 /**
@@ -114,17 +96,17 @@ void init()
  */
 void performWatering()
 {
-    if(!wateringActuator.isWatering())
+    if (!wateringActuator.isWatering())
     {
         wateringActuator.startWatering();
 
-        updateDisplay();
+        uBit.display.printAsync(drop);
 
         uBit.sleep(WATERING_TIMEOUT);
 
         wateringActuator.stopWatering();
 
-        updateDisplay();
+        uBit.display.clear();
 
         // disable forcing watering after watering
         forceWatering = false;
@@ -179,9 +161,14 @@ void onButtonAPressed(MicroBitEvent)
 
 void onButtonBPressed(MicroBitEvent)
 {
-    uBit.display.scroll("W");
+    uBit.display.scrollAsync('W');
     wateringActuator.setWateringCount(MICROBIT_WATERINGS_COUNT);
-    updateDisplay();
+}
+
+void onMoistureUpdated(MicroBitEvent)
+{
+    int32_t t = (*moistureService).getMoistureLevelTreshold();
+    uBit.display.scrollAsync((int)t);
 }
 
 int main()
@@ -193,7 +180,8 @@ int main()
     uBit.messageBus.listen(MICROBIT_ID_BLE, MICROBIT_BLE_EVT_DISCONNECTED, onDisconnected);
     uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, onButtonAPressed);
     uBit.messageBus.listen(MICROBIT_ID_WATERING_SERVICE, WATERING_EVT_REQUESTED, onWateringRequested);
-    
+    uBit.messageBus.listen(MICROBIT_ID_MOISTURE_SERVICE, MOISTURE_TRESHOLD_UPDATED, onMoistureUpdated);
+
     uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_BUTTON_EVT_CLICK, onButtonBPressed);
 
     lightService = new MicroBitLightService(*uBit.ble, uBit.display);
